a9642955cbc5585bdbe6b7c8343c894de4fc7a8d
pooyan <thepooyan@gmail.com>
2023-04-06 16:58:50 +0330

    black added

--- modules/setUpBoard.js
+++ modules/setUpBoard.js
@@ -11,8 +11,6 @@ export default function setUpBoard(Board) {
     let pawn7 = new Pawn(Board.positions.G2);
     let pawn8 = new Pawn(Board.positions.H2);
 
-    pawn1.move(Board.positions.A4.square);
-    
     let Rknight = new Knight(Board.positions.B1);
     let Lknight = new Knight(Board.positions.G1);
 
@@ -24,4 +22,25 @@ export default function setUpBoard(Board) {
 
     let king = new King(Board.positions.E1);
     let queen = new Queen(Board.positions.D1);
+
+
+    let B_pawn1 = new Pawn(Board.positions.A7, {isWhite: false});
+    let B_pawn2 = new Pawn(Board.positions.B7, {isWhite: false});
+    let B_pawn3 = new Pawn(Board.positions.C7, {isWhite: false});
+    let B_pawn4 = new Pawn(Board.positions.D7, {isWhite: false});
+    let B_pawn5 = new Pawn(Board.positions.E7, {isWhite: false});
+    let B_pawn6 = new Pawn(Board.positions.F7, {isWhite: false});
+    let B_pawn7 = new Pawn(Board.positions.G7, {isWhite: false});
+    let B_pawn8 = new Pawn(Board.positions.H7, {isWhite: false});
+
+    let B_Rknight = new Knight(Board.positions.B8, {isWhite: false});
+    let B_Lknight = new Knight(Board.positions.G8, {isWhite: false});
+    let B_Rrook = new Rook(Board.positions.A8, {isWhite: false});
+    let B_Lrook = new Rook(Board.positions.H8, {isWhite: false});
+    let B_Rbishop = new Bishop(Board.positions.C8, {isWhite: false});
+    let B_Lbishop = new Bishop(Board.positions.F8, {isWhite: false});
+    let B_king = new King(Board.positions.E8, {isWhite: false});
+    let B_queen = new Queen(Board.positions.D8, {isWhite: false});
+    
+    pawn1.move(Board.positions.A4);
 }
