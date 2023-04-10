9439be8c93f13e1ad4df6738c178ca529c2362c6
pooyan <thepooyan@gmail.com>
2023-04-10 14:25:37 +0330

    loop for file ref

--- components/TheFile.jsx
+++ components/TheFile.jsx
@@ -1,35 +1,24 @@
 import { useEffect, useRef } from "react"
 
 export default function TheFile(props) {
-    let file1 = useRef(null);
-    let file2 = useRef(null);
-    let file3 = useRef(null);
-    let file4 = useRef(null);
-    let file5 = useRef(null);
-    let file6 = useRef(null);
-    let file7 = useRef(null);
-    let file8 = useRef(null);
+    let file = [useRef(null),useRef(null),useRef(null),useRef(null),useRef(null),useRef(null),useRef(null),useRef(null)];
 
     useEffect(() => {
-        props.position[`${props.children}1`] = {square: file1.current, name: `${props.children}1`};
-        props.position[`${props.children}2`] = {square: file2.current, name: `${props.children}2`};
-        props.position[`${props.children}3`] = {square: file3.current, name: `${props.children}3`};
-        props.position[`${props.children}4`] = {square: file4.current, name: `${props.children}4`};
-        props.position[`${props.children}5`] = {square: file5.current, name: `${props.children}5`};
-        props.position[`${props.children}6`] = {square: file6.current, name: `${props.children}6`};
-        props.position[`${props.children}7`] = {square: file7.current, name: `${props.children}7`};
-        props.position[`${props.children}8`] = {square: file8.current, name: `${props.children}8`};
+        for (let i=1; i < 9; i++) {
+            props.position[`${props.children}${i}`] = {square: file[i-1].current, name: `${props.children}${i}`};
+
+        }
     })
     return (
         <div>
-            <div id={`${props.children}${8}`} ref={file8}></div>
-            <div id={`${props.children}${7}`} ref={file7}></div>
-            <div id={`${props.children}${6}`} ref={file6}></div>
-            <div id={`${props.children}${5}`} ref={file5}></div>
-            <div id={`${props.children}${4}`} ref={file4}></div>
-            <div id={`${props.children}${3}`} ref={file3}></div>
-            <div id={`${props.children}${2}`} ref={file2}></div>
-            <div id={`${props.children}${1}`} ref={file1}></div>
+            <div id={`${props.children}${8}`} ref={file[7]}></div>
+            <div id={`${props.children}${7}`} ref={file[6]}></div>
+            <div id={`${props.children}${6}`} ref={file[5]}></div>
+            <div id={`${props.children}${5}`} ref={file[4]}></div>
+            <div id={`${props.children}${4}`} ref={file[3]}></div>
+            <div id={`${props.children}${3}`} ref={file[2]}></div>
+            <div id={`${props.children}${2}`} ref={file[1]}></div>
+            <div id={`${props.children}${1}`} ref={file[0]}></div>
         </div>
     )
 }
