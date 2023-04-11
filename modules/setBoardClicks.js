ef3c031bb3cabec810e1990bc4dd5ca4479bc1e1
pooyan <thepooyan@gmail.com>
2023-04-11 06:32:59 +0330

    recent move heighlight

--- modules/setBoardClicks.js
+++ modules/setBoardClicks.js
@@ -6,12 +6,19 @@ export default function setBoardClicks(Board) {
 
             position.square.onclick = _ => {
                 let piece = position.occupent;
+                Object.values(Board.positions).forEach(pos=>{
+                    pos.square.classList.remove('selected')
+                })
                 position.square.classList.add('selected');
 
                 Object.values(Board.positions).forEach(position2 => {
                     position2.square.onclick = e => {
-                        piece.move(position2);
+                        let moved = piece.move(position2);
+                        if (moved)
+                        position2.square.classList.add('selected');
+                        else
                         position.square.classList.remove('selected');
+
                         setBoardClicks(Board);
                     }
                 })
