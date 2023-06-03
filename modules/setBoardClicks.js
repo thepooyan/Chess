1e58462fa9a5aee8c44d2e6b9b7acacde45c6c8e
pooyan <thepooyan@gmail.com>
2023-06-03 14:58:06 +0330

    click event of pieces only adding to ones of empty squares or diffrent colors

--- modules/setBoardClicks.js
+++ modules/setBoardClicks.js
@@ -6,38 +6,39 @@ export default function setBoardClicks(Board) {
 
             position.square.onclick = _ => {
                 let piece = position.occupent;
-                Object.values(Board.positions).forEach(pos=>{
+                Object.values(Board.positions).forEach(pos => {
                     pos.background.classList.remove('selected')
                 })
                 position.background.classList.add('selected');
 
                 Object.values(Board.positions).forEach(position2 => {
-                    position2.square.onclick = e => {
-                        let moved = piece.move(position2);
-                        if (moved)
-                        position2.background.classList.add('selected');
-                        else
-                        position.background.classList.remove('selected');
+                    if (position2.occupent?.isWhite !== position.occupent.isWhite)
+                        position2.square.onclick = () => {
+                            let moved = piece.move(position2);
+                            if (moved)
+                                position2.background.classList.add('selected');
+                            else
+                                position.background.classList.remove('selected');
 
-                        setBoardClicks(Board);
-                    }
+                            setBoardClicks(Board);
+                        }
                 })
             }
 
-            let x=0, y=0;
+            let x = 0, y = 0;
             function dragHandle(e) {
                 e.preventDefault();
-                
+
                 x += e.movementX;
                 y += e.movementY;
-                
+
                 position.square.style.transform = `translate(${x}px , ${y}px)`;
             }
             position.square.onmousedown = e => {
                 window.addEventListener('mousemove', dragHandle);
                 position.square.style.transition = '0s';
-                x=0; y=0;
-                
+                x = 0; y = 0;
+
                 window.onmouseup = () => {
                     window.removeEventListener('mousemove', dragHandle);
                     position.square.style.transform = null;
