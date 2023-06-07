73b14d85f2ae2fff2b2b7b86280d73b985cc72de
pooyan <thepooyan@gmail.com>
2023-06-07 13:09:05 +0330

    modal starting style and content add

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -3,10 +3,64 @@ import './Modal.scss'
 
 //contents of the modal
 const Content = () => {
+
+    const submitHandler = e => {
+        e.preventDefault();
+        alert('submit')
+    }
+
 	return (
-        <section id='modal'>
-            this is modal
-        </section>
+        <form className='content' onSubmit={submitHandler}>
+            <h3>Me</h3>
+            <label htmlFor="">
+                Name:
+                <input type="text" />
+            </label>
+            <label htmlFor="">
+                Rating:
+                <input type="number" />
+            </label>
+
+            <h3>Opponent</h3>
+            <label htmlFor="">
+                Name:
+                <input type="text" />
+            </label>
+            <label htmlFor="">
+                Rating:
+                <input type="number" />
+            </label>
+
+            <h3>i play as:</h3>
+            <label htmlFor="">
+                white
+                <input type="checkbox" />
+            </label>
+
+            <label htmlFor="">
+                black
+                <input type="checkbox" />
+            </label>
+
+            <h3>time</h3>
+
+            <label htmlFor="">
+                3 min
+                <input type="checkbox" />
+            </label>
+
+            <label htmlFor="">
+                5 min
+                <input type="checkbox" />
+            </label>
+
+            <label htmlFor="">
+                10 min
+                <input type="checkbox" />
+            </label>
+
+            <button type='submit'>Play!</button>
+        </form>
 	)
 }
 
